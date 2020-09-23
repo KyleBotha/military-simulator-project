760e143c490c0425d4feec6ca6d43a1d7457b735
Kyle Botha <kylebotha01@gmail.com>
2020-09-23 13:43:35 +0200

    Added Authenticate Form And Register Form

---
+++ military-simulator/frmRegister.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace military_simulator
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblAffiliation = new System.Windows.Forms.Label();
+            this.lblDodID = new System.Windows.Forms.Label();
+            this.txtAffiliation = new System.Windows.Forms.TextBox();
+            this.txtDodId = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblRank = new System.Windows.Forms.Label();
+            this.txtRank = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblCPassword = new System.Windows.Forms.Label();
+            this.txtCPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(180, 483);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(258, 59);
+            this.btnRegister.TabIndex = 11;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Army", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(68, 27);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(350, 38);
+            this.lblTitle.TabIndex = 10;
+            this.lblTitle.Text = "Military Simulator";
+            // 
+            // lblAffiliation
+            // 
+            this.lblAffiliation.AutoSize = true;
+            this.lblAffiliation.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAffiliation.Location = new System.Drawing.Point(86, 289);
+            this.lblAffiliation.Name = "lblAffiliation";
+            this.lblAffiliation.Size = new System.Drawing.Size(105, 19);
+            this.lblAffiliation.TabIndex = 9;
+            this.lblAffiliation.Text = "Affiliation";
+            // 
+            // lblDodID
+            // 
+            this.lblDodID.AutoSize = true;
+            this.lblDodID.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDodID.Location = new System.Drawing.Point(86, 243);
+            this.lblDodID.Name = "lblDodID";
+            this.lblDodID.Size = new System.Drawing.Size(59, 19);
+            this.lblDodID.TabIndex = 8;
+            this.lblDodID.Text = "DoD ID";
+            // 
+            // txtAffiliation
+            // 
+            this.txtAffiliation.BackColor = System.Drawing.Color.Olive;
+            this.txtAffiliation.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAffiliation.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAffiliation.Location = new System.Drawing.Point(282, 285);
+            this.txtAffiliation.Name = "txtAffiliation";
+            this.txtAffiliation.PasswordChar = '*';
+            this.txtAffiliation.Size = new System.Drawing.Size(234, 25);
+            this.txtAffiliation.TabIndex = 7;
+            // 
+            // txtDodId
+            // 
+            this.txtDodId.BackColor = System.Drawing.Color.Olive;
+            this.txtDodId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDodId.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDodId.Location = new System.Drawing.Point(283, 239);
+            this.txtDodId.Name = "txtDodId";
+            this.txtDodId.Size = new System.Drawing.Size(234, 25);
+            this.txtDodId.TabIndex = 6;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(86, 102);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(52, 19);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.Olive;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtName.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(283, 98);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(234, 25);
+            this.txtName.TabIndex = 12;
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSurname.Location = new System.Drawing.Point(86, 148);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(83, 19);
+            this.lblSurname.TabIndex = 15;
+            this.lblSurname.Text = "Surname";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.BackColor = System.Drawing.Color.Olive;
+            this.txtSurname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSurname.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(282, 144);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(234, 25);
+            this.txtSurname.TabIndex = 14;
+            // 
+            // lblRank
+            // 
+            this.lblRank.AutoSize = true;
+            this.lblRank.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRank.Location = new System.Drawing.Point(86, 196);
+            this.lblRank.Name = "lblRank";
+            this.lblRank.Size = new System.Drawing.Size(53, 19);
+            this.lblRank.TabIndex = 17;
+            this.lblRank.Text = "Rank";
+            // 
+            // txtRank
+            // 
+            this.txtRank.BackColor = System.Drawing.Color.Olive;
+            this.txtRank.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtRank.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRank.Location = new System.Drawing.Point(282, 192);
+            this.txtRank.Name = "txtRank";
+            this.txtRank.Size = new System.Drawing.Size(234, 25);
+            this.txtRank.TabIndex = 16;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(86, 336);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(92, 19);
+            this.lblUsername.TabIndex = 19;
+            this.lblUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.Olive;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(282, 332);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.PasswordChar = '*';
+            this.txtUsername.Size = new System.Drawing.Size(234, 25);
+            this.txtUsername.TabIndex = 18;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(86, 382);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(91, 19);
+            this.lblPassword.TabIndex = 21;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Olive;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(282, 378);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(234, 25);
+            this.txtPassword.TabIndex = 20;
+            // 
+            // lblCPassword
+            // 
+            this.lblCPassword.AutoSize = true;
+            this.lblCPassword.Font = new System.Drawing.Font("Army", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPassword.Location = new System.Drawing.Point(86, 428);
+            this.lblCPassword.Name = "lblCPassword";
+            this.lblCPassword.Size = new System.Drawing.Size(164, 19);
+            this.lblCPassword.TabIndex = 23;
+            this.lblCPassword.Text = "Confirm Password";
+            // 
+            // txtCPassword
+            // 
+            this.txtCPassword.BackColor = System.Drawing.Color.Olive;
+            this.txtCPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCPassword.Font = new System.Drawing.Font("Army", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPassword.Location = new System.Drawing.Point(282, 424);
+            this.txtCPassword.Name = "txtCPassword";
+            this.txtCPassword.PasswordChar = '*';
+            this.txtCPassword.Size = new System.Drawing.Size(234, 25);
+            this.txtCPassword.TabIndex = 22;
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(593, 572);
+            this.Controls.Add(this.lblCPassword);
+            this.Controls.Add(this.txtCPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblRank);
+            this.Controls.Add(this.txtRank);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblAffiliation);
+            this.Controls.Add(this.lblDodID);
+            this.Controls.Add(this.txtAffiliation);
+            this.Controls.Add(this.txtDodId);
+            this.Name = "frmRegister";
+            this.Text = "frmRegister";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblAffiliation;
+        private System.Windows.Forms.Label lblDodID;
+        private System.Windows.Forms.TextBox txtAffiliation;
+        private System.Windows.Forms.TextBox txtDodId;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblRank;
+        private System.Windows.Forms.TextBox txtRank;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblCPassword;
+        private System.Windows.Forms.TextBox txtCPassword;
+    }
+}
