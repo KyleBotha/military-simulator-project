445588d9c8288bcab17ca3f20a43c72bbd5479ac
Kyle Botha <kylebotha01@gmail.com>
2020-09-25 17:15:13 +0200

    Started building simulator form

--- military-simulator/frmSimulator.Designer.cs
+++ military-simulator/frmSimulator.Designer.cs
@@ -28,21 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSimulator));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlRight = new System.Windows.Forms.Panel();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.pbArmyCamp = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArmyCamp)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 800);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pnlRight
+            // 
+            this.pnlRight.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.pnlRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlRight.Controls.Add(this.label1);
+            this.pnlRight.Controls.Add(this.pbArmyCamp);
+            this.pnlRight.Location = new System.Drawing.Point(800, 0);
+            this.pnlRight.Name = "pnlRight";
+            this.pnlRight.Size = new System.Drawing.Size(350, 900);
+            this.pnlRight.TabIndex = 1;
+            this.pnlRight.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlRight_Paint);
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBottom.Location = new System.Drawing.Point(0, 800);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(800, 100);
+            this.pnlBottom.TabIndex = 2;
+            this.pnlBottom.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBottom_Paint);
+            // 
+            // pbArmyCamp
+            // 
+            this.pbArmyCamp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbArmyCamp.Image = ((System.Drawing.Image)(resources.GetObject("pbArmyCamp.Image")));
+            this.pbArmyCamp.InitialImage = null;
+            this.pbArmyCamp.Location = new System.Drawing.Point(36, 54);
+            this.pbArmyCamp.Name = "pbArmyCamp";
+            this.pbArmyCamp.Size = new System.Drawing.Size(104, 99);
+            this.pbArmyCamp.TabIndex = 0;
+            this.pbArmyCamp.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Army", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click to choose which item to drop";
             // 
             // frmSimulator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.ClientSize = new System.Drawing.Size(1119, 619);
+            this.ClientSize = new System.Drawing.Size(1150, 900);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.pnlRight);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmSimulator";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmSimulator";
+            this.Load += new System.EventHandler(this.frmSimulator_Load);
+            this.pnlRight.ResumeLayout(false);
+            this.pnlRight.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArmyCamp)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlRight;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.PictureBox pbArmyCamp;
+        private System.Windows.Forms.Label label1;
     }
 }
